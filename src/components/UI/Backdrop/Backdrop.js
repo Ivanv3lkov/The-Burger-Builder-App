29db69342f6cfb4a62d436c0bd508485147a8d2c
ivanv3lkov <ivanv3lkov@gmail.com>
2020-12-04 23:26:34 +0200

    Burger Builder Update

--- src/components/UI/Backdrop/Backdrop.js
+++ src/components/UI/Backdrop/Backdrop.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import classes from './Backdrop.css';
 
 const backdrop = (props) => (
--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import classes from './Button.css';
 
 const button = (props) => (

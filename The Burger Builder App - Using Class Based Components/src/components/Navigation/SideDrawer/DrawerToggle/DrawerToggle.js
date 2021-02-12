634238426e8a5788f80fc98f4b2d4f0ff51dce5c
Ivan Velkov <ivanv3lkov@gmail.com>
2021-02-12 18:04:01 +0200

    The Burger Builder App - Using Class Based Components

--- The Burger Builder App - Using Class Based Components/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
+++ The Burger Builder App - Using Class Based Components/src/components/Navigation/SideDrawer/DrawerToggle/DrawerToggle.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import classes from './DrawerToggle.css';
 
-const drawerToggle = (props) => (
+const drawerToggle = props => (
     <div className={classes.DrawerToggle} onClick={props.clicked}>
         <div></div>
         <div></div>
